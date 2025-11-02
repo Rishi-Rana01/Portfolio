c9b95faa0d102a5b26bd7b4ab413dfe376e94173
Rishi Rana <rishirana012354@gmail.com>
2025-11-02 22:10:08 +0530

    Skills initial

---
+++ my-portfolio/src/components/Skills.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Frontend from '/Frontend.png'
+
+const skillsData=[
+    {id:1, image:Frontend.png, title:'Frontend',description: 'HTML,CSS, JS,REACT, NEXT.JS TAILWIND CSS,BOOTSTRAP'},
+    {id:2, image:backend.png, title:'Backend',description: 'NODE.JS,EXPRESS.JS,PYTHON,DJANGO,REST'},
+    {id:3, image:database.png, title:'Database',description: 'MONGODB,MONGOOSE'},
+    {id:4, image:Ai.png, title:'AI',description: 'GERNATIVE AI, PANDAS, NUMPY'},
+]
+const Skills = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Skills
